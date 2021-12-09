ee2e9087b02a6e81f9a8f6a2646c77f737e83ace
Mehul <mehulprajapati2802@gmail.com>
2021-12-08 19:50:58 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+setTimeout(() => {console.log("this is the first message")}, 5000);
+setTimeout(() => {console.log("this is the second message")}, 3000);
+setTimeout(() => {console.log("this is the third message")}, 1000);
