debb838a013af4ece2408ffcfd42b115d7d891d1
Mehul Prajapati <cug05241@staff.concordia.ca>
2021-12-10 20:18:05 -0500

    bootstrap init

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/views", '/index.html'));
